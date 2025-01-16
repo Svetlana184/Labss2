dce13a69ab6036c6a3935763490e4a806ac835b2
Svetlana184 <sveta.kgd.39@mail.com>
2025-01-16 20:42:49 +0200

    (^ _ ^)

--- LAB_1.1/Program.cs
+++ LAB_1.1/Program.cs
@@ -1,159 +1,212 @@
-﻿try
+﻿
+try
 {
     Console.WriteLine("введите действительную часть первого числа:");
-    int x = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите мнимую часть первого числа:");
-    int x_mn = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите действительную часть первого числа:");
-    int y = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите мнимую часть первого числа:");
-    int y_mn = int.Parse(Console.ReadLine()!);
-    Complex complex = new Complex()
-    {
-        X = x,
-        Y = y,
-        X_mn = x_mn,
-        Y_mn = y_mn
-    };
-    Console.WriteLine(complex);
-
-    Console.WriteLine("введите числитель первого числа:");
     int a = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите знаменатель первого числа:");
+    Console.WriteLine("введите мнимую часть первого числа:");
     int b = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите числитель первого числа:");
+    Console.WriteLine("введите действительную часть второго числа:");
     int c = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("введите знаменатель первого числа:");
+    Console.WriteLine("введите мнимую часть второго числа:");
     int d = int.Parse(Console.ReadLine()!);
-    Rational rational = new Rational()
+    Complex complex1 = new Complex
     {
         X = a,
-        Y = b,
-        X_zm = c,
-        Y_zm = d
+        Y = b
     };
-    Console.WriteLine(rational.Reduce(a, b));
+    Complex complex2 = new Complex
+    {
+        X = c,
+        Y = d
+    };
+    Console.WriteLine("операции над комплексными числами:");
+    Complex rez1 = complex1 + complex2;
+    Console.WriteLine($"({complex1.X},{complex1.Y}) + ({complex2.X},{complex2.Y}) = ({rez1.X},{rez1.Y})");
+    Complex rez2 = complex1 - complex2;
+    Console.WriteLine($"({complex1.X},{complex1.Y}) - ({complex2.X},{complex2.Y}) = ({rez2.X},{rez2.Y})");
+    Complex rez3 = complex1 * complex2;
+    Console.WriteLine($"({complex1.X},{complex1.Y}) * ({complex2.X},{complex2.Y}) = ({rez3.X},{rez3.Y})");
+    Complex rez4 = complex1 / complex2;
+    Console.WriteLine($"({complex1.X},{complex1.Y}) / ({complex2.X},{complex2.Y}) = ({rez4.X},{rez4.Y})");
+    Console.WriteLine($"({complex1.X},{complex1.Y}) = ({complex2.X},{complex2.Y}) - {complex1 == complex2}");
+    Complex rez5 = complex1.Conj(complex1);
+    Console.WriteLine($"сопряженное число для ({complex1.X},{complex1.Y}) - ({rez5.X}, {rez5.Y})");
+    Complex rez6 = complex2.Conj(complex2);
+    Console.WriteLine($"сопряженное число для ({complex2.X},{complex2.Y}) - ({rez6.X}, {rez6.Y})");
+    Console.WriteLine();
+
+    Console.WriteLine("введите числитель первого числа:");
+    a = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("введите знаменатель первого числа:");
+    b = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("введите числитель второго числа:");
+    c = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("введите знаменатель второго числа:");
+    d = int.Parse(Console.ReadLine()!);
+    Rational rational1 = new Rational
+    {
+        X = a,
+        Y = b
+    };
+    Rational rational2 = new Rational
+    {
+        X = c,
+        Y = d
+    };
+
+    Console.WriteLine("операции над рациональными дробями:");
+    Rational rez7 = rational1 + rational2;
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) + ({rational2.X}/{rational2.Y}) = ({rez7.X}/{rez7.Y})");
+    Rational rez8 = rational1 - rational2;
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) - ({rational2.X}/{rational2.Y}) = ({rez8.X}/{rez8.Y})");
+    Rational rez9 = rational1 * rational2;
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) * ({rational2.X}/{rational2.Y}) = ({rez9.X}/{rez9.Y})");
+    Rational rez10 = rational1 / rational2;
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) / ({rational2.X}/{rational2.Y}) = ({rez10.X}/{rez10.Y})");
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) = {rational2.X}/{rational2.Y}) - ({rational1 == rational2}");
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) > {rational2.X}/{rational2.Y}) - ({rational1 > rational2}");
+    Console.WriteLine($"({rational1.X}/{rational1.Y}) < {rational2.X}/{rational2.Y}) - ({rational1 < rational2}");
 }
 catch
 {
-    Console.WriteLine("введите переменные правильно");
+    Console.WriteLine("Введите переменные правильно");
 }
 abstract class Pair
 {
     public int X { get; set; }
     public int Y { get; set; }
-    public abstract (int, int) Add();
-    public abstract (int, int) Subtract();
-    public abstract (int, int) Multiply();
-    public abstract (int, int) Divide();
-    public abstract bool Equal();
 }
 
 class Complex : Pair
 {
-    public int X_mn { get; set; }
-    public int Y_mn { get; set; }
-    public override (int, int) Add() => (X + X_mn, Y + Y_mn);
-
-    public override (int, int) Divide()
+    public static Complex operator +(Complex c1, Complex c2)
     {
-        return ((X * Y + X_mn * Y_mn), (X * Y_mn + X_mn * Y));
+        return new Complex 
+        { 
+            X = c1.X + c1.Y,
+            Y = c2.X + c2.Y
+        };
+    }
+    public static Complex operator -(Complex c1, Complex c2)
+    {
+        return new Complex
+        {
+            X = c1.X - c1.Y,
+            Y = c2.X - c2.Y
+        };
+    }
+    public static Complex operator /(Complex c1, Complex c2)
+    {
+        return new Complex
+        {
+            X = c1.X * c2.X + c1.Y * c2.Y,
+            Y = c1.X * c2.Y + c1.Y * c2.X
+        };
+    }
+    public static Complex operator *(Complex c1, Complex c2)
+    {
+        return new Complex
+        {
+            X = (c1.X * c2.X - c1.Y * c2.Y)/(c2.X * c2.X + c2.Y * c2.Y),
+            Y = (c1.Y * c2.X - c1.X * c2.Y) / (c2.X * c2.X + c2.Y * c2.Y)
+        };
+    }
+    public static bool operator ==(Complex c1, Complex c2)
+    {
+        return c1.X == c2.X && c1.Y == c2.Y;
+    }
+    public static bool operator !=(Complex c1, Complex c2)
+    {
+        return c1.X != c2.X && c1.Y != c2.Y;
+    }
+    public Complex Conj(Complex c)
+    {
+        return new Complex
+        {
+            X = c.X,
+            Y = -c.Y,
+        };
     }
 
-    public override bool Equal() => X == Y && X_mn == Y_mn;
-
-    public override (int, int) Multiply()
-    {
-        return ((X * Y - X_mn * Y_mn) / Y * Y + Y_mn * Y_mn, (X_mn * Y - X * Y_mn) / Y * Y + Y_mn * Y_mn);
-    }
-
-    public override (int, int) Subtract() => (X - X_mn, Y - Y_mn);
-    public (int, int) Conj(int a, int b) => (a, -b);
-    public override string? ToString()
-    {
-        return $"({X}, {X_mn}) + ({Y}, {Y_mn}) = {Add()}\n" +
-            $"({X}, {X_mn}) - ({Y}, {Y_mn}) = {Subtract()}\n" +
-            $"({X}, {X_mn}) * ({Y}, {Y_mn}) = {Multiply()}\n" +
-            $"({X}, {X_mn}) / ({Y}, {Y_mn}) = {Divide()}\n" +
-            $"({X}, {X_mn}) = ({Y}, {Y_mn}) - {Equal()}\n" +
-            $"Сопряженное число для ({X}, {X_mn}) - {Conj(X, X_mn)}\n" +
-            $"Сопряженное число для ({Y}, {Y_mn}) - {Conj(Y, Y_mn)}";
-    }
 }
 
 class Rational : Pair
 {
-    public int X_zm { get; set; }
-    public int Y_zm { get; set; }
-    public override (int, int) Add()
+    private static void Reduce(Rational r)
     {
-        Reduce(X, X_zm);
-        return ((X * Y_zm + X_zm + Y), X_zm * Y_zm);
-    }
-
-    public override (int, int) Divide()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return (X * Y_zm, Y * X_zm);
-    }
-
-    public override bool Equal()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return X == Y && X_zm == Y_zm;
-    }
-
-    public override (int, int) Multiply()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return (X * Y, X_zm + Y_zm);
-    }
-
-    public override (int, int) Subtract()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return ((X * Y_zm - X_zm + Y), X_zm * Y_zm);
-    }
-
-    public bool Greate()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return ((double)X / X_zm) > ((double)Y / Y_zm);
-    }
-
-    public bool Less()
-    {
-        Reduce(X, X_zm);
-        Reduce(Y, Y_zm);
-        return ((double)X / X_zm) < ((double)Y / Y_zm);
-    }
-
-    public (int, int) Reduce(int a, int b)
-    {
-        int c = b;
-        while (a != 1 || b != 1)
+        int c = r.Y;
+        while (r.X >= 1 && r.Y >= 1 && c >= 1)
         {
-            if (a % c == 0)
+            if (r.X % c == 0 && r.Y % c == 0)
             {
-                a = a / c;
-                b = b / c;
+                r.X = r.X / c;
+                r.Y = r.Y / c;
             }
             c--;
         }
-        return (a, b);
     }
-
-    public override string? ToString()
+    public static Rational operator +(Rational r1, Rational r2)
     {
-        return $"({X}, {X_zm}) + ({Y}, {Y_zm}) = {Add()}\n" +
-            $"({X}, {X_zm}) - ({Y}, {Y_zm}) = {Subtract()}\n" +
-            $"({X}, {X_zm}) * ({Y}, {Y_zm}) = {Multiply()}\n" +
-            $"({X}, {X_zm}) / ({Y}, {Y_zm}) = {Divide()}\n" +
-            $"({X}, {X_zm}) = ({Y}, {Y_zm}) - {Equal()}\n";
+        Reduce(r1);
+        Reduce(r2);
+        return new Rational
+        {
+            X = r1.X * r2.Y + r1.Y * r2.X,
+            Y = r1.Y * r2.Y
+        };
+    }
+    public static Rational operator -(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return new Rational
+        {
+            X = r1.X * r2.Y - r1.Y * r2.X,
+            Y = r1.Y * r2.Y
+        };
+    }
+    public static Rational operator *(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return new Rational
+        {
+            X = r1.X * r2.X,
+            Y = r1.Y * r2.Y
+        };
+    }
+    public static Rational operator /(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return new Rational
+        {
+            X = r1.X * r2.Y,
+            Y = r1.Y * r2.X
+        };
+    }
+    public static bool operator ==(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return r1.X == r2.X && r1.Y == r2.Y;
+    }
+    public static bool operator !=(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return r1.X != r2.X && r1.Y != r2.Y;
+    }
+    public static bool operator >(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return ((double)r1.X / r1.Y) > ((double)r2.X / r2.Y);
+    }
+    public static bool operator <(Rational r1, Rational r2)
+    {
+        Reduce(r1);
+        Reduce(r2);
+        return ((double)r1.X / r1.Y) < ((double)r2.X / r2.Y);
     }
 }

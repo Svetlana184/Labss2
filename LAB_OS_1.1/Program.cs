7caba34e55d77db0c263b07288e905155dd3914c
Svetlana184 <sveta.kgd.39@mail.com>
2025-04-30 13:43:17 +0200

    os1

---
+++ LAB_OS_1.1/Program.cs
@@ -0,0 +1,83 @@
+﻿Mutex mutexObj = new();
+Console.Write("Введите название файла:");
+string path = Console.ReadLine()!;
+string path_2 = "простые_числа.txt";
+string path_3 = "простые_числа_c_семёрками.txt";
+Thread myThread1 = new(Task1Async);
+myThread1.Start();
+Thread myThread2 = new(Task2);
+myThread2.Start();
+Thread myThread3 = new(Task3);
+myThread3.Start();
+
+
+void Task1Async()
+{
+    mutexObj.WaitOne();
+    Console.WriteLine("выполняется запись первого файла...");
+    string text = "";
+    Random random = new Random();
+    for(int i =0; i < random.Next(10, 20); i++)
+    {
+        text += random.Next(1, 100) + " ";
+    }
+    text += "67 ";
+    using (StreamWriter writer = new StreamWriter(path, false))
+    {
+        writer.WriteLine(text);
+    }
+    mutexObj.ReleaseMutex();
+}
+void Task2()
+{
+    mutexObj.WaitOne();
+    Console.WriteLine("выполняется запись второго файла...");
+    string text = "";
+    using (StreamReader reader = new StreamReader(path))
+    {
+        text = reader.ReadToEnd();
+    }
+    int[] mas = text.Split(' ').SkipLast(1).Select(int.Parse).ToArray();
+    string new_text = "";
+    for(int i =0; i<mas.Length; i++)
+    {
+        int x = mas[i];
+        bool simple = true;
+        for(int j = 2;  j <= x/2; j++)
+        {
+            if (x % j == 0)
+            {
+                simple = false;
+                break;
+            }
+        }
+        if (simple) new_text += x + " ";
+    }
+    using (StreamWriter writer = new StreamWriter(path_2, false))
+    {
+        writer.WriteLine(new_text);
+    }
+    mutexObj.ReleaseMutex();
+}
+void Task3()
+{
+    mutexObj.WaitOne();
+    Console.WriteLine("выполняется запись третьего файла...");
+    string text = "";
+    using (StreamReader reader = new StreamReader(path))
+    {
+        text = reader.ReadToEnd();
+    }
+    int[] mas = text.Split(' ').SkipLast(1).Select(int.Parse).ToArray();
+    string new_text = "";
+    for (int i = 0; i < mas.Length; i++)
+    {
+        int x = mas[i];
+        if (x % 10 == 7) new_text += x + " ";
+    }
+    using (StreamWriter writer = new StreamWriter(path_3, false))
+    {
+        writer.WriteLine(new_text);
+    }
+    mutexObj.ReleaseMutex();
+}
---
+++ LAB_OS_1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB_OS_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 0;
+            string text = "";
+            //Mutex mutexObj = new();
+            string path_1 = "fileone.txt";
+            // запускаем пять потоков
+            //for (int i = 1; i < 3; i++)
+            //{
+
+            //}
+
+            Create_FileAsync();
+            Easy_File();
+
+            async Task Create_FileAsync()
+            {
+                //mutexObj.WaitOne();     // приостанавливаем поток до получения мьютекса
+                using (StreamWriter writer = new StreamWriter(path_1, false))
+                {
+                    Random random = new Random();
+                    for(int i = 0;  i < random.Next(10, 50); i++)
+                    {
+                        text += random.Next(1, 50) + " ";
+                    }
+                    await writer.WriteLineAsync(text);
+                }
+                //mutexObj.ReleaseMutex();    // освобождаем мьютекс
+            }
+
+            async Task Easy_File()
+            {
+                string text_fileone;
+                using (StreamReader reader = new StreamReader(path_1))
+                {
+                    text_fileone = await reader.ReadToEndAsync();
+                }
+                int[] mas = text_fileone.Split(' ').Select(x => int.Parse(x)).ToArray();
+                Text_output.Text = mas.ToString();
+            }
+        }
+    }
+}
